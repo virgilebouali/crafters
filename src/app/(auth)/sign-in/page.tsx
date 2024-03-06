e3fb3b4a649f9cb2aebbd7aaa66b2c462785293a
virgilebouali <virgile.bouali@gmail.com>
2024-03-06 23:17:30 +0100

    google auth

--- src/app/(auth)/sign-in/page.tsx
+++ src/app/(auth)/sign-in/page.tsx
@@ -6,14 +6,14 @@ const Page = () => {
   return (
     <main className="bg-popover max-w-lg mx-auto my-4 rounded-lg p-10">
       <h1 className="text-2xl font-bold text-center">
-        Sign in to your account
+        Se connecter
       </h1>
       <div className="mt-4">
         <button
           onClick={() => signIn(undefined, { callbackUrl: "/dashboard" })}
           className="w-full bg-primary text-primary-foreground text-center hover:opacity-90 font-medium px-4 py-2 rounded-lg block"
         >
-          Sign In
+          Connexion
         </button>
       </div>
     </main>
